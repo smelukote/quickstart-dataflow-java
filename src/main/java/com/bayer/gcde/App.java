cf9d7986177906bd82931cfebb0dd58a09d6e43b
smelukote <smelukote@gmail.com>
2022-01-13 15:26:27 -0800

    First commit

---
+++ src/main/java/com/bayer/gcde/App.java
@@ -0,0 +1,13 @@
+package com.bayer.gcde;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
